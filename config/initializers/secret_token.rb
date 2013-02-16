fbc500454ee1c117cf9883244ed7eaf3b8c86435
Bill Mers <bill.w.mers@gmail.com>
2013-02-16 03:38:13 -0800

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MersWeb::Application.config.secret_token = 'e1e98563c1a9fa5a25697f11d50ca809f919e28ee4c5d72c92c57b4935e6be2bcf74fcd2c5c9d663f0c2b59b7b5fc8298f63ec42286af4524b05fc28e8dc2a4b'
